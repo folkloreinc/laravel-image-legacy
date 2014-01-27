b695aa5f93a747e14b1a9564dd2311a0d86cec1e
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2014-01-26 22:50:40 -0500

    Added image.js javascript helper

---
+++ public/image.js
@@ -0,0 +1,59 @@
+// --------------------------------------------------
+// Utilities for working with Image
+// --------------------------------------------------
+define(function (require) {
+
+	var URL_PARAMETER = '-image({options})';
+	
+	// Build a image formatted URL
+	function url(src, width, height, options) {
+
+		// Don't allow empty strings
+		if (!src || !src.length) return;
+
+		//If width parameter is an array, use it as options
+		if(width instanceof Object)
+		{
+			options = width;
+			width = null;
+			height = null;
+		}
+
+		//Get size
+		if (!width) width = '_';
+		if (!height) height = '_';
+		
+		// Produce the image option
+		var params = [];
+		params.push(width+'x'+height);
+		
+		// Add options.
+		if (options && options instanceof Object) {
+			var val, key;
+			for (key in options) {
+				val = options[key];
+				if (val === true || val === null) {
+					params.push(key);
+				}
+				else if (val instanceof Array) {
+					params.push(key+'('+val.join(',')+')');
+				}
+				else {
+					params.push(key+'('+val+')');
+				}
+			}
+		}
+		
+		params = params.join('-');
+		var parameter = URL_PARAMETER.replace('{options}',params);
+
+		// Break the path apart and put back together again
+		return src.replace(/^(.+)(\.[a-z]+)$/i, "$1"+parameter+"$2");
+		
+	}
+	
+	// Expose public methods.
+	return {
+		url: url
+	};
+});
