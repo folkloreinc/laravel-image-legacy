12cec767f66ae111c881778637d91f13cf7172e5
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2014-02-03 21:08:25 -0500

    Basic unit testing

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -1,8 +1,135 @@
 <?php
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Folklore\Image\Exception\FormatException;
+
 class ImageTest extends Orchestra\Testbench\TestCase {
 
-	protected function getPackageProviders()
+    protected $imagePath = '/image.jpg';
+    protected $imageSize;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        Image::deleteManipulated($this->imagePath);
+
+        $this->imageSize = getimagesize(public_path().$this->imagePath);
+    }
+
+
+
+    public function testURLisValid()
+    {
+
+        $options = array(
+            'grayscale',
+            'crop' => true,
+            'colorize' => 'FFCC00'
+        );
+        $url = Image::url($this->imagePath, 300, 300, $options);
+
+        //Check against pattern
+        $urlMatch = preg_match('#'.Image::getPattern().'#',$url,$matches);
+        $this->assertEquals($urlMatch,1);
+
+        //Check path
+        $parsedPath = Image::parse($url);
+        $this->assertEquals($parsedPath['path'],$this->imagePath);
+
+        //Check options
+        foreach($options as $key => $value) {
+            if(is_numeric($key)) {
+                $this->assertTrue($parsedPath['options'][$value]);
+            } else {
+                $this->assertEquals($parsedPath['options'][$key], $value);
+            }
+        }
+    }
+
+    public function testServeNoResize()
+    {
+        $url = Image::url($this->imagePath,null,null,array(
+            'grayscale'
+        ));
+        $response = $this->call('GET', $url);
+
+        $this->assertTrue($response->isOk());
+
+        $sizeManipulated = getimagesizefromstring($response->getContent());
+        $this->assertEquals($sizeManipulated[0],$this->imageSize[0]);
+        $this->assertEquals($sizeManipulated[1],$this->imageSize[1]);
+    }
+
+    public function testServeResizeWidth()
+    {
+        $url = Image::url($this->imagePath,300);
+        $response = $this->call('GET', $url);
+
+        $this->assertTrue($response->isOk());
+
+        $sizeManipulated = getimagesizefromstring($response->getContent());
+        $this->assertEquals($sizeManipulated[0],300);
+    }
+
+    public function testServeResizeHeight()
+    {
+        $url = Image::url($this->imagePath,null,300);
+        $response = $this->call('GET', $url);
+
+        $this->assertTrue($response->isOk());
+
+        $sizeManipulated = getimagesizefromstring($response->getContent());
+        $this->assertEquals($sizeManipulated[1],300);
+    }
+
+    public function testServeResizeCrop()
+    {
+        //Both height and width with crop
+        $url = Image::url($this->imagePath,300,300,array(
+            'crop' => true
+        ));
+        $response = $this->call('GET', $url);
+
+        $this->assertTrue($response->isOk());
+
+        $sizeManipulated = getimagesizefromstring($response->getContent());
+        $this->assertEquals($sizeManipulated[0],300);
+        $this->assertEquals($sizeManipulated[1],300);
+    }
+
+    public function testServeWrongParameter()
+    {
+        $url = Image::url($this->imagePath,300,300,array(
+            'crop' => true,
+            'wrong' => true
+        ));
+        $this->setExpectedException('Symfony\Component\HttpKernel\Exception\NotFoundHttpException');
+        $response = $this->call('GET', $url);
+    }
+
+    public function testServeWrongFile()
+    {
+        $url = Image::url('/wrong123.jpg',300,300,array(
+            'crop' => true,
+            'wrong' => true
+        ));
+        $this->setExpectedException('Symfony\Component\HttpKernel\Exception\NotFoundHttpException');
+        $response = $this->call('GET', $url);
+    }
+
+    public function testServeWrongFormat()
+    {
+        $url = Image::url('/wrong.jpg',300,300,array(
+            'crop' => true
+        ));
+        $this->setExpectedException('Folklore\Image\Exception\FormatException');
+        $response = $this->call('GET', $url);
+    }
+
+
+
+    protected function getPackageProviders()
     {
         return array('Folklore\Image\ImageServiceProvider');
     }
@@ -14,25 +141,16 @@ class ImageTest extends Orchestra\Testbench\TestCase {
         );
     }
 
-    public function setUp()
+    protected function getApplicationPaths()
     {
-        parent::setUp();
+        $basePath = realpath(__DIR__.'/../vendor/orchestra/testbench/src/fixture');
 
-        
-    }
-
-    public function testImageURLisValid()
-    {
-
-        $url = Image::url('/images/photo.jpg',300,300,array(
-        	'grayscale',
-        	'crop' => true,
-        	'colorize' => 'FFCC00'
-        ));
-
-        $urlMatch = preg_match('#'.Image::getPattern().'#',$url,$matches);
-
-        $this->assertEquals($urlMatch,1);
+        return array(
+            'app'     => "{$basePath}/app",
+            'public'  => realpath(__DIR__.'/fixture'),
+            'base'    => $basePath,
+            'storage' => "{$basePath}/app/storage",
+        );
     }
 
 }
