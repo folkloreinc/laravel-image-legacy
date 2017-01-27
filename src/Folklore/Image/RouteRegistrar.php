1a456efe8c31d699c2d94c049452059b0a2efb40
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2017-01-27 18:03:03 -0500

    Fix with dependencies

--- src/Folklore/Image/RouteRegistrar.php
+++ src/Folklore/Image/RouteRegistrar.php
@@ -18,6 +18,12 @@ class RouteRegistrar
         'as', 'domain', 'middleware', 'name', 'namespace', 'prefix',
     ];
 
+    protected $patternName = 'image_pattern';
+
+    protected $cacheMiddleware = 'image.middleware.cache';
+
+    protected $controller = '\Folklore\Image\Http\ImageController@serve';
+
     /**
      * Create a new route registrar instance.
      *
@@ -36,20 +42,28 @@ class RouteRegistrar
         $domain = array_get($config, 'domain', null);
         $cache = array_get($config, 'cache', false);
         $middleware = array_get($config, 'middleware', []);
+        $patternName = array_get($config, 'pattern_name', $this->cacheMiddleware);
+        $cacheMiddleware = array_get($config, 'cache_middleware', $this->cacheMiddleware);
+        $controller = array_get($config, 'uses', $this->controller);
 
         if ($cache) {
-            $middleware[] = 'image.middleware.cache';
+            $middleware[] = $cacheMiddleware;
         }
 
         // Here we check if the route contains any url config. If it does, we
         // create a route pattern to catch it
         $patternOptions = array_get($config, 'url', []);
         if (sizeof($patternOptions)) {
-            $patternName = 'image_pattern_'.preg_replace('/[^a-z0-9]+/i', '_', preg_replace('/^image\./', '', $as));
+            $generatedPatternName = 'image_pattern_'.preg_replace(
+                '/[^a-z0-9]+/i',
+                '_',
+                preg_replace('/^image\./', '', $as)
+            );
+            $patternName = array_get($config, 'pattern_name', $generatedPatternName);
             $routePattern = $this->container['image.url']->pattern($patternOptions);
             $this->router->pattern($patternName, $routePattern);
         } else {
-            $patternName = 'image_pattern';
+            $patternName = array_get($config, 'pattern_name', $this->patternName);
         }
 
         $routePath = preg_replace('/\{\s*pattern\s*\}/i', '{'.$patternName.'}', $path);
@@ -59,7 +73,7 @@ class RouteRegistrar
             'domain' => $domain,
             'middleware' => $middleware,
             'image' => array_except($config, $this->allowedAttributes),
-            'uses' => '\Folklore\Image\Http\ImageController@serve'
+            'uses' => $controller
         ));
     }
 }
--- src/config/image.php
+++ src/config/image.php
@@ -131,56 +131,9 @@ return array(
     |
     */
     'routes' => [
-        'default' => [
-            // The path of the route. {pattern} will be replaced by the url
-            // pattern for this route according to the url format.
-            'route' => '{pattern}',
-
-            // A domain that will be used by the route
-            'domain' => null,
-
-            // Any middleware you want ot add on the route.
-            'middleware' => [],
-
-            // The name of the source to get the image. If it is set to null,
-            // it will get use the default source.
-            'source' => null,
-
-            // Allow to specify a size as filter
-            'allow_size' => true,
-
-            // Allow to specify filters in url. You can also set this to
-            // an array of specific filters to restrict this route to those
-            // filters.
-            //
-            // Example: ["negative"]
-            'allow_filters' => true,
-
-            // Disallow some filters. Can be set to an array of filters.
-            'disallow_filters' => false,
-
-            // Any url options you want to override.
-            'url' => [],
-
-            // You can specify base filters that will be applied to any image
-            // on this route.
-            'filters' => [
-                'width' => 100
-            ],
-
-            // Expires header in seconds
-            'expires' => 3600 * 24 * 31,
-
-            // Any headers you want to add on the image
-            'headers' => [],
-
-            // Cache the file on local machine
-            'cache' => true,
-
-            // The path where the images are cached. It is defined to public
-            // path, so the files would be statically served on next request.
-            'cache_path' => public_path()
-        ]
+        'pattern_name' => 'image_pattern',
+        'controller' => '\Folklore\Image\Http\ImageController@serve',
+        'cache_middleware' => 'image.middleware.cache'
     ]
 
 );
