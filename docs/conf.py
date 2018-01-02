251415284506a11139ddb134d5b2a83567609896
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-01-02 18:01:28 -0500

    Fix with autostructify

--- docs/conf.py
+++ docs/conf.py
@@ -176,8 +176,4 @@ texinfo_documents = [
 
 # At the bottom of conf.py
 def setup(app):
-    app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: github_doc_root + url,
-            'auto_toc_tree_section': 'Contents',
-            }, True)
     app.add_transform(AutoStructify)
