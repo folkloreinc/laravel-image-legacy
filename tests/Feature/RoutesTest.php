439e18856231ee1ac594c199d358c665d9414fb4
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-01-03 21:21:56 -0500

    Moved tests into 2 separate folders for feature and unit testing

---
+++ tests/Feature/RoutesTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ *
+ */
+class RoutesTest extends TestCase
+{
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app['config']->set('image.routes', [
+            // Path to the routes file that will be automatically mapped. Set to null
+            // to prevent auto-loading of routes.
+            'map' => null,
+
+            // Default domain for routes
+            'domain' => null,
+
+            // Default namespace for controller
+            'namespace' => null,
+
+            // Default middlewares for routes
+            'middleware' => [],
+
+            // The controller serving the images
+            'controller' => '\Folklore\Image\Http\ImageController@serve',
+
+            // The name of the pattern that will be added to the Laravel Router.
+            'pattern_name' => 'image_pattern',
+
+            // The middleware used when a route as cache enabled
+            'cache_middleware' => 'image.middleware.cache'
+        ]);
+    }
+
+    /**
+     * Test routes with specific filters
+     *
+     * @test
+     */
+    public function testRoutesFilters()
+    {
+        $this->app['router']->image('thumbnail/{pattern}', [
+            'as' => 'image.thumbnail',
+            'filters' => [
+                'width' => 100,
+                'height' => 100,
+                'crop' => true,
+            ],
+        ]);
+
+        $url = image()->url('image.jpg', [
+            'route' => 'image.thumbnail'
+        ]);
+        $this->assertEquals('http://localhost/thumbnail/image.jpg', $url);
+
+        $patterns = $this->app['router']->getPatterns();
+        $this->assertRegExp('/'.$patterns['image_pattern'].'/', 'image.jpg');
+
+        $response = $this->call('GET', $url);
+        // dd($response);
+    }
+}
